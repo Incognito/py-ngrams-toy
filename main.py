0c6599ad931ce217381fe2924519e3a930d72bb6
Brian Graham <drakain@gmail.com>
2015-07-26 15:43:24 -0400

    Now we count sets instead of ordered tuples.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from textblob import TextBlob
+from collections import Counter
 import pprint
 
 def main(text):
-    tuples = get_tupels(lower)
-    pprint.pprint(tuples)
+    tuples = get_tupels(text)
+    counted = count_word_pairs(tuples)
+    pprint.pprint(counted)
 
 def get_tupels(text):
     lower = text.lower()
-    blob = TextBlob(text)
-    return map(tuple, blob.ngrams(n=2))
+    blob = TextBlob(lower)
+    ngrams = blob.ngrams(n=2)
+    tuples = map(tuple,map(tuple, ngrams))
+    return tuples
 
-def count_word_pairs():
-    return []
+def count_word_pairs(tupleList):
+    return Counter((frozenset(item)) for item in tupleList)
